3fa4733077b3f30756fdbaf43de366ee45200127
willm93 <willmarkcum93@gmail.com>
2024-04-22 18:29:39 -0400

    reworked movement to avoid input deceleration
    connected bodies no longer move the player properly so they aren't supported for now

--- Assets/Scripts/Player/AbilityController.cs
+++ Assets/Scripts/Player/AbilityController.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class AbilityController : MonoBehaviour
 {
-    [SerializeField, Range(0f, 100f)] float dashSpeed = 50f;
-    [SerializeField, Min(0)] int stepsPerDash = 20;
-    int stepsDashing;
+    [SerializeField, Range(0f, 100f)] float dashForce = 50f;
     PlayerController pc;
     Rigidbody body;
     Transform orientation;
-    Vector3 desiredVelocity, dashDirection;
-    float initSpeed, currentSpeed;
-    bool dashTried, dashing;
+    Vector3 dashDirection;
+    bool dashing;
 
     void Awake()
     {
@@ -19,52 +16,25 @@ public class AbilityController : MonoBehaviour
         orientation = transform.Find("Orientation");
     }
 
-    void FixedUpdate()
-    {
-        desiredVelocity = body.velocity;
-        currentSpeed = pc.HorizontalSpeed;
-
-        if (dashTried && !dashing) {
-            initSpeed = currentSpeed;
-            dashing = true;
-            dashTried = false;
-        }
-
-        if (dashing) {
-            stepsDashing += 1;
-            float intoWallDot = Vector3.Dot(dashDirection, -pc.WallNormal);
-            if (currentSpeed < dashSpeed && stepsDashing <= stepsPerDash) {
-                float speedDelta = dashSpeed - currentSpeed;
-                speedDelta *= Mathf.Clamp01(1 - intoWallDot);
-                desiredVelocity += pc.ProjectOnPlane(speedDelta * dashDirection, pc.GroundNormal);
-            }
-
-            desiredVelocity.y *= Mathf.Clamp01(1 - 0.18f * intoWallDot);
-
-            if (stepsDashing > stepsPerDash) {
-                dashing = false;
-                stepsDashing = 0;
-                if (pc.HorizontalSpeed > initSpeed) {
-                    float speedDelta = pc.HorizontalSpeed - initSpeed;
-                    desiredVelocity -= pc.ProjectOnPlane(speedDelta * desiredVelocity.normalized, pc.GroundNormal);
-                    desiredVelocity.y = 0; //stops wierd super high bounces
-                }
-            }
-        }
-
-        body.velocity = desiredVelocity;
-    }
-
     public void TryDash(Vector3 direction)
     {
-        if (!dashing) {
-            dashTried = true;
+        /*if (!dashing) {
+            dashing = true;
             if (direction == Vector3.zero) {
                 dashDirection = orientation.forward;
             }
             else {
                 dashDirection = orientation.forward * direction.z + orientation.right * direction.x;
             }
-        }
+
+            body.AddForce(dashForce * dashDirection, ForceMode.Impulse);
+
+            Invoke(nameof(ResetDash), 1f);
+        }*/
+    }
+
+    void ResetDash()
+    {
+        dashing = false;
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,10 +6,10 @@ public class PlayerController: MonoBehaviour
     [Header("Speeds/Accels")]
     [SerializeField, Range(0f, 100f)] float baseSpeed = 10f, climbSpeed = 3f;
     [SerializeField, Range(0f, 200f)] float baseAccel = 30f, airAccel = 10f, climbAccel = 12f, airSpeedGroundDecel = 50f;
-    [SerializeField, Range(0f, 10f)] float jumpHeight = 2f;
+    [SerializeField, Range(0f, 15f)] float jumpHeight = 2f, groundDrag = 10f;
     [SerializeField, Min(0)] int stepsForAirSpeed = 12, stepsTilJumpIgnored = 12;
-    float horizontalSpeed, airSpeed, jumpSpeed;
-    public float HorizontalSpeed => horizontalSpeed;
+    float flatSpeed, jumpSpeed;
+    public float FlatSpeed => flatSpeed;
     bool jumpTried, climbTried;
     int stepsSinceLastGrounded, stepsSinceLastJump, stepsSinceJumpTried;
 
@@ -34,11 +34,10 @@ public class PlayerController: MonoBehaviour
     public bool Climbing => climbContactCount > 0 && stepsSinceLastJump > 2;
 
     Transform orientation;
-    Rigidbody body, connectedBody, previousConnectedBody;
-    Vector3 desiredVelocity, inputDirection, connectedVelocity;
-    Vector3 connectionWorldPos, connectionLocalPos;
+    Rigidbody body;
+    Vector3 desiredVelocity, inputDirection;
 
-    Vector3 zAxis, xAxis, usedNormal;
+    Vector3 zAxis, xAxis, currentNormal;
     float currentSpeed, currentAccel;
 
     void Awake() 
@@ -59,7 +58,6 @@ public class PlayerController: MonoBehaviour
         minClimbFacingAwayDot = Mathf.Cos(maxclimbFacingAwayAngle * Mathf.Deg2Rad);
 
         jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
-        airSpeed = baseSpeed;
     }
 
     public void SetInputDirection(Vector3 direction) 
@@ -85,10 +83,13 @@ public class PlayerController: MonoBehaviour
 
     void FixedUpdate() 
     {
+        desiredVelocity = body.velocity;
         UpdateState();
-        SetAxis();
+        SetMovementAxis();
         SetSpeedAndAccel();
         EvaluateInputDirection();
+        CustomDrag();
+        LimitSpeed();
 
         if (jumpTried) {
             Jump();
@@ -97,10 +98,9 @@ public class PlayerController: MonoBehaviour
             desiredVelocity -= climbNormal * (climbAccel * Time.deltaTime * 0.9f);
             desiredVelocity += -Physics.gravity * Time.deltaTime;
         } 
-        else if (OnGround && desiredVelocity.sqrMagnitude < 0.01f) {
+        else if (OnGround && inputDirection == Vector3.zero) {
             desiredVelocity += -Physics.gravity * Time.deltaTime;
         }
-        
         body.velocity = desiredVelocity;
         ClearState();
     }
@@ -110,29 +110,13 @@ public class PlayerController: MonoBehaviour
         stepsSinceLastGrounded += 1;
         stepsSinceLastJump += 1;
         stepsSinceJumpTried += 1;
-        desiredVelocity = body.velocity;
 
         if (Climbing || OnGround || SnapToGround()) {
             stepsSinceLastGrounded = 0;
-                
-            if (groundContactCount > 1) 
-                groundNormal.Normalize();
-            
-            if (climbContactCount > 1)
-                climbNormal.Normalize();
-
             previousWallNormal = Vector3.zero;
         }
         else {
-            if (wallContactCount > 1)
-                wallNormal.Normalize();
-
             groundNormal = Vector3.up;
-        }
-
-        if (connectedBody) {
-            if (connectedBody.isKinematic || connectedBody.mass >= body.mass)
-                UpdateConnectionState();
         }
     }
     
@@ -153,7 +137,6 @@ public class PlayerController: MonoBehaviour
 
         groundContactCount = 1;
         groundNormal = hit.normal;
-        connectedBody = hit.rigidbody;
         float dot = Vector3.Dot(desiredVelocity, hit.normal);
         if (dot > 0) //if velocity points upward away from plane
             desiredVelocity = (desiredVelocity - hit.normal * dot).normalized * speed;
@@ -161,71 +144,43 @@ public class PlayerController: MonoBehaviour
         return true;
     }
 
-    void UpdateConnectionState()
-    {
-        if (connectedBody == previousConnectedBody) {
-            Vector3 connectedDisplacement = connectedBody.transform.TransformPoint(connectionLocalPos) - connectionWorldPos;
-            connectedVelocity = connectedDisplacement / Time.deltaTime;
-        }
-        connectionWorldPos = body.position;
-        connectionLocalPos = connectedBody.transform.InverseTransformPoint(connectionWorldPos);
-    }
-
-    void SetAxis()
+    void SetMovementAxis()
     {
         xAxis = orientation.right;
         zAxis = orientation.forward;
-        usedNormal = groundNormal;
+        currentNormal = groundNormal;
 
         if (Climbing) {
             xAxis = Vector3.Cross(climbNormal, Vector3.up);
             zAxis = Vector3.up;
-            usedNormal = climbNormal;
+            currentNormal = climbNormal;
         }
 
-        xAxis = ProjectOnPlane(xAxis, usedNormal).normalized;
-        zAxis = ProjectOnPlane(zAxis, usedNormal).normalized;
+        xAxis = Vector3.ProjectOnPlane(xAxis, currentNormal).normalized;
+        zAxis = Vector3.ProjectOnPlane(zAxis, currentNormal).normalized;
     }
 
     void SetSpeedAndAccel()
     {
-        horizontalSpeed = ProjectOnPlane(body.velocity, usedNormal).magnitude;
+        flatSpeed = Vector3.ProjectOnPlane(body.velocity, Vector3.up).magnitude;
 
         if (Climbing) {
             currentSpeed = climbSpeed;
             currentAccel = climbAccel;
         }   
         else if (OnGround) {
-            if (airSpeed > baseSpeed) {
-                airSpeed = Mathf.Max(airSpeed - airSpeedGroundDecel * Time.deltaTime, baseSpeed);
-                if (horizontalSpeed < airSpeed) {
-                    airSpeed = horizontalSpeed;
-                }
-                currentSpeed = airSpeed;
-            } 
-            else {
-                currentSpeed = baseSpeed;
-            }
+            currentSpeed = baseSpeed;
             currentAccel = baseAccel;
         } 
         else {
-            if (horizontalSpeed > airSpeed && stepsSinceLastJump < stepsForAirSpeed) {
-                airSpeed = horizontalSpeed;
-            }
-            else if (horizontalSpeed < baseSpeed) {
-                airSpeed = baseSpeed;
-            }
-            else if (horizontalSpeed < airSpeed) {
-                airSpeed = horizontalSpeed;
-            }
-            currentSpeed = airSpeed;
+            currentSpeed = baseSpeed;
             currentAccel = airAccel;
         }
     }
 
     void EvaluateInputDirection() 
     {
-        Vector3 relativeVelocity = desiredVelocity - connectedVelocity;
+        /*Vector3 relativeVelocity = desiredVelocity - connectedVelocity;
         float currentX = Vector3.Dot(relativeVelocity, xAxis);
         float currentZ = Vector3.Dot(relativeVelocity, zAxis);
                     
@@ -233,7 +188,37 @@ public class PlayerController: MonoBehaviour
         float newX = Mathf.MoveTowards(currentX, inputDirection.x * currentSpeed, speedChange);
         float newZ = Mathf.MoveTowards(currentZ, inputDirection.z * currentSpeed, speedChange);
 
-        desiredVelocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
+        desiredVelocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);*/
+
+        float xDelta = inputDirection.x * currentAccel * Time.deltaTime;
+        float zDelta = inputDirection.z * currentAccel * Time.deltaTime;
+        
+        desiredVelocity += xAxis * xDelta + zAxis * zDelta;
+    }
+
+    void CustomDrag()
+    {
+        if (OnGround && inputDirection == Vector3.zero) {
+            body.drag = groundDrag;
+        }
+        else {
+            body.drag = 0;
+        }
+    }
+
+    void LimitSpeed() 
+    {
+        if (Climbing || OnGround) {
+            if (desiredVelocity.sqrMagnitude > currentSpeed * currentSpeed) {
+                desiredVelocity = desiredVelocity.normalized * currentSpeed;
+            }
+        }
+        else {
+            if (flatSpeed > currentSpeed) {
+                Vector3 limitedVelocity = desiredVelocity.normalized * currentSpeed;
+                desiredVelocity = new Vector3(limitedVelocity.x, desiredVelocity.y, limitedVelocity.z);
+            }
+        }   
     }
 
     void Jump() 
@@ -269,10 +254,6 @@ public class PlayerController: MonoBehaviour
     {
         groundContactCount = wallContactCount = climbContactCount = 0;
         groundNormal = wallNormal = climbNormal = Vector3.zero;
-        
-        previousConnectedBody = connectedBody;
-        connectedBody = null;
-        connectedVelocity = Vector3.zero;
     }
 
     void OnCollisionEnter(Collision collision) 
@@ -295,15 +276,12 @@ public class PlayerController: MonoBehaviour
             if (normal.y >= minGroundDot) {
                 groundContactCount += 1;
                 groundNormal += normal;
-                connectedBody = collision.rigidbody;
             } 
             else {
                 //wall contact
                 if (normal.y > -0.01f) {
                     wallContactCount += 1;
                     wallNormal += normal;
-                    if (groundContactCount == 0)
-                        connectedBody = collision.rigidbody;
                 }
                 //climbing contact
                 if (climbTried && 
@@ -313,15 +291,18 @@ public class PlayerController: MonoBehaviour
                 ){
                     climbContactCount += 1;
                     climbNormal += normal;
-                    connectedBody = collision.rigidbody;
                 }
             }
         }
-    }
+        if (groundContactCount > 1) 
+            groundNormal.Normalize();
+        
+        if (climbContactCount > 1)
+            climbNormal.Normalize();
 
-    public Vector3 ProjectOnPlane(Vector3 vector, Vector3 normal) 
-    {
-        return vector - normal * Vector3.Dot(vector, normal);
+        if (wallContactCount > 1)
+            wallNormal.Normalize();
+
     }
 
     public bool FacingWall(Vector3 facingDir, Vector3 wallNormal, float minAngleCosine)
